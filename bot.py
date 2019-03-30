869f30aecd21ada0c5c57f15540a4ff321bbbe58
FilArt <filart97@gmail.com>
2019-03-30 13:50:51 +0300

    sentry integration

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
+import sentry_sdk
+
 from telegram.ext import Updater
 
 from config import TOKEN, REQUEST_KWARGS
 from handlers import handling_handlers
 from jobs import run_jobs
 from selenium_client import SeleniumClient
+
+sentry_sdk.init("https://feaba2f28b9f4a078fa57026f5b5b356@sentry.io/1427389")
 
 updater = Updater(token=TOKEN, request_kwargs=REQUEST_KWARGS, workers=10)
 job_queue = updater.job_queue
